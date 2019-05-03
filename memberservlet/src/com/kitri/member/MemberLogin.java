8d39eff567cfe1322e079c056e2e69f45d39e948
sohyoun <mongswell69@naver.com>
2019-05-03 18:41:08 +0900

    05/03

--- memberservlet/src/com/kitri/member/MemberLogin.java
+++ memberservlet/src/com/kitri/member/MemberLogin.java
@@ -1,6 +1,10 @@
 package com.kitri.member;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,9 +16,86 @@ import javax.servlet.http.HttpServletResponse;
 public class MemberLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
+	@Override
+	public void init(ServletConfig config) throws ServletException {//3)
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");//4)
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
 	//보안때문에 post
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("로그인하러 왔다.");
+		Connection conn = null;			//5)
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+//		1. data get (아이디, 비번)
+		request.setCharacterEncoding("utf-8");//2)
+		
+		String id = request.getParameter("id");		//1)
+		String pass = request.getParameter("pass");
+		
+//		2. Logic : 1의 결과에 따라 data select
+//		select name
+//		from member
+//		where id = ?
+//		and pass = ?
+		
+		String name = null;//13)
+		try {
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.14.52:1521:orcl", "kitri", "kitri");//6)
+			StringBuffer sql = new StringBuffer();//7)
+			sql.append("select name\n");//8)
+			sql.append("from member\n");
+			sql.append("where id = ?\n");
+			sql.append("and pass = ?\n");
+			
+			pstmt = conn.prepareStatement(sql.toString()); //9)
+			pstmt.setString(1, id);		//10)
+			pstmt.setString(2, pass);
+			rs = pstmt.executeQuery();//11)
+			if(rs.next()) {//12)
+				name = rs.getString("name");//14)
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {//15)
+			try {
+				if(rs!=null)
+					rs.close();
+				if(pstmt!=null)
+					pstmt.close();
+				if(conn!=null)
+					conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+//		3. response : 2의 결과에 따라
+//			3-1. name != null : 홍길동님 안녕하세요
+//			3-2. name == null : 아이디 또는 비밀번호를 다시 확인하세요.
+//								 등록되지 않은 아이디이거나, 아이디 또는 비밀번호를 잘못 입력하셨습니다.
+//								 로그인
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println(" 	<body>");
+		if(name != null) {
+			out.println("<strong>"+name+"</strong>님 안녕하세요.<br>");
+		}else {
+			out.println("<font size=\"13\" color=\"red\">");
+			out.println("아이디 또는 비밀번호를 다시 확인하세요.<br>");
+			out.println("등록되지 않은 아이디이거나, 아이디 또는 비밀번호를 잘못 입력하셨습니다.");
+			out.println("</font>");
+			out.println("<a href=\"/memberservlet/user/login.html\">로그인</a>");
+		}
+		out.println(" 	</body>");
+		out.println("</html>");
 	}
 
 }
--- memberservlet/src/com/kitri/member/MemberRegister.java
+++ memberservlet/src/com/kitri/member/MemberRegister.java
@@ -1,6 +1,10 @@
 package com.kitri.member;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,10 +15,107 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/register")
 public class MemberRegister extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {//3)init override 할 때 config 가져오는 놈으로 하기!
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");//4)
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	//보안때문에 post로 해야함
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("회원가입하러왔다");
+		//5)
+		Connection conn = null;//선언은 맨 위에
+		PreparedStatement pstmt = null;
+
+//		1. data get (이름, 아이디, 비번, 이메일1, 이메일2, 전번1, 전번2, 전번3, 우편번호, 주소, 상세주소)
+		request.setCharacterEncoding("UTF-8");//한글 깨짐 방지	2)
+		//1)
+		String name = request.getParameter("name");
+		String id = request.getParameter("id");
+		String pass = request.getParameter("pass");
+		String emailid = request.getParameter("emailid");
+		String emaildomain = request.getParameter("emaildomain");
+		String tel1 = request.getParameter("tel1");
+		String tel2 = request.getParameter("tel2");
+		String tel3 = request.getParameter("tel3");
+		String zipcode = request.getParameter("zipcode");
+		String address = request.getParameter("address");
+		String addressdetail = request.getParameter("address_detail");
+
+//		2. Logic : 1의 data를 DB에 insert
+//		insert all
+//			into member(id,name,pass,emailid,emaildomain,joindate)
+//			values(?,?,?,?,?,sysdate)
+//			into memberdetail(id,zipcode,address,address_detail,tel1,tel2,tel3)
+//			values(?,?,?,?,?,?,?)
+//		select * from dual
+		int cnt=0; //8)
+		try {
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.14.52:1521:orcl", "kitri", "kitri");
+			StringBuffer sql = new StringBuffer(); //가능하면 sql은 String buffer로!
+			sql.append("insert all\n");//6) editplus에서 작성 
+			sql.append("	into member(id,name,pass,emailid,emaildomain,joindate)\n");
+			sql.append("	values(?,?,?,?,?,sysdate)\n");
+			sql.append("	into member_detail(id,zipcode,address,address_detail,tel1,tel2,tel3)\n");
+			sql.append("	values(?,?,?,?,?,?,?)\n");
+			sql.append("select * from dual\n");
+			pstmt = conn.prepareStatement(sql.toString());//preparedstatement는 sql문장을 먼저 가져간다. StringBuffer를 String으로 바꿔야함.
+			int idx=0;
+			pstmt.setString(++idx, id);//7)
+			pstmt.setString(++idx, name);
+			pstmt.setString(++idx, pass);
+			pstmt.setString(++idx, emailid);
+			pstmt.setString(++idx, emaildomain);
+			pstmt.setString(++idx, id);
+			pstmt.setString(++idx, zipcode);
+			pstmt.setString(++idx, address);
+			pstmt.setString(++idx, addressdetail);
+			pstmt.setString(++idx, tel1);
+			pstmt.setString(++idx, tel2);
+			pstmt.setString(++idx, tel3);//물음표 갯수와 일치해야함
+			
+			cnt = pstmt.executeUpdate();//9) preparedstatement는 여기선 sql문장 안가져감
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {//10)
+			try {
+				if(pstmt!=null)
+					pstmt.close();
+				if(conn!=null)
+					conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+//		3. response page : 2의 결과에 따라
+//			3-1. !0 : (성공) 홍길동님 회원가입을 환영합니다.	>>쿼리가 몇개일 지 모름으로 1일때,2일때 하는 것이 아니라 0이 아닐 때
+//			3-2. 0	: (실패) 서버 문제로 회원 가입이 실패하였습니다.
+//							다음에 다시 시도하세요.
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println(" 	<body>");
+		if(cnt!=0) {
+			out.println(name+"님 회원가입을 환영합니다.<br>");
+			out.println("로그인 후 모든 서비스를 이용할 수 있습니다.<br>");
+			out.println("<a href=\"/memberservlet/user/login.html\">로그인</a>");
+		}else {
+			out.println("<font size=\"13\" color=\"red\">");
+			out.println("서버문제로 회원가입이 실패하였습니다.<br>");
+			out.println("다음에 다시 시도하세요.");
+			out.println("</font>");
+		}
+		out.println(" 	</body>");
+		out.println("</html>");
+		
 	}
+
+	
 
 }
